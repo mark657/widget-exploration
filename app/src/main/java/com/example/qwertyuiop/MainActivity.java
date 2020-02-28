5ad46aa22a00e8c2f285dd23266882bc6b3e78ad
mark657 <markpicak@gmail.com>
2020-02-28 21:05:16 +0500

    новые проблемы

--- app/src/main/java/com/example/qwertyuiop/MainActivity.java
+++ app/src/main/java/com/example/qwertyuiop/MainActivity.java
@@ -9,6 +9,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.exploration_layout);
+
+        final RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        final RadioButton radioButtonLondon = (RadioButton) findViewById(R.id.radioButtonLondon);
+        // дальше самостоятельно
     }
 }
